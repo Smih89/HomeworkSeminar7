882c682b554d901d3422432d2976c25cd7629653
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-01-11 14:53:23 +0300

    add Task1 png

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,6 +2,25 @@
 // которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
 
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// a = 5, b = 7 -> max = 7
+// a = 2, b =10 -> max = 10
+// a =-9, b =-3 -> max = -3
+
+int a = 5;
+int b = 7;
+int max = a;
+int min = b;
+if (b > max)
+{
+  max = b;
+  min = a;
+}
+else
+  max = a;
+  min = b;
+Console.WriteLine(max);
+Console.WriteLine("max");
+Console.WriteLine(min);
+Console.WriteLine("min");
+
+
